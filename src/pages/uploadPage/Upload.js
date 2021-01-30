68bdad0b43eb540bcf698adfb709d27245705c1e
Tommy Krijt <tommykrijt@gmail.com>
2021-01-30 20:34:22 +0100

    created form and added css.

--- src/pages/uploadPage/Upload.js
+++ src/pages/uploadPage/Upload.js
@@ -8,9 +8,28 @@ function Upload() {
         <>
             <div className="page-container">
                 <Header/>
-                <div className="form-container">
-                    <p className="dummy-text">This is the upload page</p>
+                <div className="upload-form-container">
+                    <form className="upload-form">
+                        <div className="form-item">
+                            <label className="form-title">name</label>
+                            <input className="form-input" type="text" name="name" />
+                        </div>
+                        <div className="form-item">
+                            <label className="form-title">email</label>
+                            <input className="form-input" type="text" name="email" />
+                        </div>
+                        <div className="form-item">
+                            <label className="form-title">file</label>
+                            <input className="form-input" type="text" name="file" />
+                        </div>
+                        <div className="form-item">
+                            <label className="form-title">comment</label>
+                            <textarea className="form-input-comment" name="comment" />
+                        </div>
+                        <button type="submit" value="Submit">submit</button>
+                    </form>
                 </div>
+
                 <Footer/>
             </div>
         </>
